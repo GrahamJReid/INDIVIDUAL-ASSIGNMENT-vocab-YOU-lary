677b63ed6d985a04201a0bc379d5084b214e2543
GrahamJReid <grahamreidtechnology@gmail.com>
2022-11-26 11:09:10 -0600

    starting

---
+++ src/scripts/utils/startApp.js
@@ -0,0 +1,13 @@
+import domBuilder from '../components/shared/domBuilder';
+import navBar from '../components/shared/navBar';
+
+const startApp = () => {
+  domBuilder(); // BUILD THE DOM
+  // ADD THE EVENT LISTENTERS TO THE DOM
+  // ADD FORM EVENT LISTENTERS TO THE DOM
+  navBar(); // DYNAMICALLY ADD THE NAV
+  // ADD THE LOGOUT BUTTON COMPONENT
+  // ATTACH THE EVENT LISTENERS TO THE NAVBAR
+};
+export default startApp;
+startApp();
