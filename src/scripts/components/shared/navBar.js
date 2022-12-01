8f0f1fe0c2e357e24ed289c91c82c3ae46ca4979
GrahamJReid <grahamreidtechnology@gmail.com>
2022-12-01 13:19:55 -0600

    added logo

--- src/scripts/components/shared/navBar.js
+++ src/scripts/components/shared/navBar.js
@@ -2,11 +2,11 @@ import renderToDOM from '../../utils/renderToDOM';
 
 const navBar = () => {
   const domString = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#"><i class="fas fa-braille navbar-logo"></i></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-  
+    
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
